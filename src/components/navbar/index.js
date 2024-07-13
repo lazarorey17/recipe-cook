054382a8b654ef1903a372dad9317086d5f7703c
lazarorey17 <lazarorey17@gmail.com>
2024-07-13 09:09:24 -0400

    Initial

---
+++ src/components/navbar/index.js
@@ -0,0 +1,11 @@
+const Navbar = () =>{
+    return(
+        <div>
+            <p>Recipe name</p>
+            <input placeholder="Search recipe"/>
+        </div>
+    );
+}
+
+export default Navbar;
+
